e3b6d3a3cebe1814d3af9c1cdd1b66c0fc0fcfa7
강지원 <kjason08@kaist.ac.kr>
2023-08-23 16:34:59 +0900

    test20

--- path-to-your-js-file.js
+++ path-to-your-js-file.js
@@ -21,7 +21,7 @@ function calculateRoute() {
   const request = {
     origin: start,
     destination: end,
-    travelMode: google.maps.TravelMode['BICYCLING']
+    travelMode: google.maps.TravelMode[1]
   };
 
   directionsService.route(request, function(response, status) {
